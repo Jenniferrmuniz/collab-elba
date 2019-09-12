b300133f87c8da4de6a0ffb7ebb3720973b7b33e
Elba Chimilio <elbachimilio@gmail.com>
2019-09-12 14:35:13 -0400

    fixed time/date placement

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 var today = new Date();
 var date = (today.getMonth()+1)+'-'+today.getDate()+'-'+today.getFullYear();
-// var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 var time = today.getHours()%12 + ":" + today.getMinutes() + ":" + today.getSeconds();
 var dateTime = date+' '+time;
-document.write(dateTime);
-// console.log('Date is: ', dateTime);
+document.querySelector("body > h3").innerHTML += dateTime;
